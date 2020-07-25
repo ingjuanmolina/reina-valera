68960099353bd29833907735c572e0e7704b1ef0
Juan Molina <ing.jcmolina@gmail.com>
2020-07-25 15:52:50 -0500

    Thymeleaf added

--- src/main/java/com/bible/reinavalera/controller/HomeController.java
+++ src/main/java/com/bible/reinavalera/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.bible.reinavalera.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
-        return "Welcome to home";
+    public String home(Model model) {
+        model.addAttribute("name", "Juan");
+        return "home";
     }
 }
