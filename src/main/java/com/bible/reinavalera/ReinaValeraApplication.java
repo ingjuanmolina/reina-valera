a1420831fd25a930c6706d51feabc6cf5c63e444
juancarlosmolinaglobant <juancarlos.molina@globant.com>
2021-08-19 18:45:06 -0500

    Rename main class

--- src/main/java/com/bible/reinavalera/ReinaValeraApplication.java
+++ src/main/java/com/bible/reinavalera/ReinaValeraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReinavaleraApplication {
+public class ReinaValeraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReinavaleraApplication.class, args);
+		SpringApplication.run(ReinaValeraApplication.class, args);
 	}
 
 }
