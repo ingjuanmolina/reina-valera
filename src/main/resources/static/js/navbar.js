a442516b4c53c4922764f2a81bbef76dc5462590
Juan Molina <ing.jcmolina@gmail.com>
2021-10-08 18:59:05 -0500

    navbar updated

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -2,14 +2,14 @@ const navbar = document.querySelector('.nav');
 const navbarHTML = `
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="http://localhost:8080/home">Inicio</a>
+        <a class="navbar-brand" href="/home">Inicio</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link" href="http://localhost:8080/books">Libros</a>
-                <a class="nav-item nav-link" href="http://localhost:8080/search">Buscar</a>
+                <a class="nav-item nav-link" href="/books">Libros</a>
+                <a class="nav-item nav-link" href="/search">Buscar</a>
             </div>
         </div>
     </nav>
